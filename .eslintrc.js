21340fd9c4437a86ebf0d40b081696bf7c2d1f70
thor <thor@wolpert.ca>
2021-06-07 22:28:47 -0700

    added some rules, changed the lint to insclude ts as per the nuxt docs (#7)

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,9 @@ module.exports = {
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': ['warn', { code: 120, ignoreRegExpLiterals: true }],
+  },
 }
