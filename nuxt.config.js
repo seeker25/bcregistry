3e4751ef1f6a2a2e1e7d4b2d5e0e1b11021d4f26
Stephanie Rabbani <jordiwes@gmail.com>
2022-03-01 09:48:02 -0800

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'BC Registries and Online Services',
+    title: 'BC Registries Dashboard',
     htmlAttrs: {
       lang: 'en',
     },
