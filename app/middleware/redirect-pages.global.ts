b924f9f996af951f52bf0241c01bf8f463c152e2
deetz99 <73151365+deetz99@users.noreply.github.com>
2025-03-21 12:04:48 -0700

    Fixes (#228)
    * add missing vault variable
    
    * add await to prevent 404 page
    
    * use login from local keycloak, not relying on auth-web
    
    * version
    
    * add timeout to signout route

--- app/middleware/redirect-pages.global.ts
+++ app/middleware/redirect-pages.global.ts
@@ -6,7 +6,7 @@ enum RedirectPaths {
   SIGNOUT = '/signout'
 }
 
-export default defineNuxtRouteMiddleware((to) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const redirectPaths = Object.values(RedirectPaths) as string[]
 
   // remove trailing slash if it exists
@@ -19,18 +19,25 @@ export default defineNuxtRouteMiddleware((to) => {
     const registryDashboardUrl = rtc.baseUrl + locale + '/dashboard'
     const logoutRedirectUrl = sessionStorage.getItem('LOGOUT_URL') || registryLoginUrl
     const loginUrl = sessionStorage.getItem('LOGIN_URL') || registryDashboardUrl
+    const { login, logout } = useKeycloak()
 
     switch (path) {
       case RedirectPaths.FILING:
         return navigateTo(rtc.supportFilingUrl, { external: true })
       case RedirectPaths.SIGNOUT:
-        return useKeycloak().logout(logoutRedirectUrl)
+        await logout(logoutRedirectUrl)
+        return new Promise(resolve => setTimeout(resolve, 300)) // wait for logout process to complete
       case RedirectPaths.SIGNIN_BCEID:
-        return navigateTo(rtc.authWebURL + 'signin/bceid/' + encodeURIComponent(loginUrl), { external: true })
+        return await login(IdpHint.BCEID, loginUrl)
       case RedirectPaths.SIGNIN_BCSC:
-        return navigateTo(rtc.authWebURL + 'signin/bcsc/' + encodeURIComponent(loginUrl), { external: true })
+        return await login(IdpHint.BCSC, loginUrl)
       case RedirectPaths.SIGNIN_IDIR:
-        return navigateTo(rtc.authWebURL + 'signin/idir/' + encodeURIComponent(loginUrl), { external: true })
+        return await login(IdpHint.IDIR, loginUrl)
     }
   }
 })
+
+// Leaving this here in case we need to switch back
+// return navigateTo(rtc.authWebURL + 'signin/bceid/' + encodeURIComponent(loginUrl), { external: true })
+// return navigateTo(rtc.authWebURL + 'signin/bcsc/' + encodeURIComponent(loginUrl), { external: true })
+// return navigateTo(rtc.authWebURL + 'signin/idir/' + encodeURIComponent(loginUrl), { external: true })
